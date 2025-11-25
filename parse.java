ecf4ec41f1801b002493a7efc048d62b0547cf97
TheRudraBro <rudramraj22@gmail.com>
2025-11-25 23:08:00 +0600

    Uppdated

---
+++ parse.java
@@ -0,0 +1,7 @@
+public class parse {
+    public static void main(String[] args) {
+        String numberString = "12345";
+        int number = Integer.parseInt(numberString);
+        System.out.println("Parsed Number: " + number);
+    }
+}
