3e3989fd93ec9cabf740055622499f21f13f3542
TheRudraBro <rudramraj22@gmail.com>
2025-11-25 23:06:21 +0600

    Updated

---
+++ substring.java
@@ -0,0 +1,7 @@
+public class substring {
+    public static void main(String[] args) {
+        String sentence = "My name is John Doe";
+        String name = sentence.substring(11, 19);
+        System.out.println("Extracted Name: " + name);
+    }
+}
