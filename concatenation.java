50520f7071b41866d5ab1c26cfd908f200206999
TheRudraBro <rudramraj22@gmail.com>
2025-11-25 22:03:52 +0600

    Updated

---
+++ concatenation.java
@@ -0,0 +1,8 @@
+public class concatenation {
+    public static void main(String[] args) {
+        String firstName = "Jane";
+        String lastName = "Doe";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
+}
