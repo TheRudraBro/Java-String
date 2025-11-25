9f8c75fae29f185b4a9598992c433bce73fd3517
TheRudraBro <rudramraj22@gmail.com>
2025-11-25 21:53:23 +0600

    first commit

---
+++ string_declare.java
@@ -0,0 +1,14 @@
+public class string_declare {
+
+public static void main(String[] args) {
+
+// string declaration
+String name = "John";
+String fullName = "John Doe";
+System.out.println("Name: " + name);
+String sentence = "Hello, my name is " + name + ".";
+System.out.println(sentence);
+System.out.println("Full Name: " + fullName);
+    
+}
+}
